636e24a1b5c245c61d882d0942748f74a646d9b8
bhanu <lorenbhanu@gmail.com>
2026-02-20 12:53:36 +0530

    no idea what i did but this is working upto loading file content as string

--- main.c
+++ main.c
@@ -1,15 +1,24 @@
-#include "file.h"
-#include "directory.h"
-#include "string.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include "file.h"
+#include "directory.h"
+#include "string.h"
+#include "vector.h"
 
 int main(int argc, char **argv) {
     string *command = create_string();
     for (int i = 0; i < argc; i++) {
-        if (i == 0) {
-            string_append_str(command, "gcc");//append gcc command for the first argument
+        if(i==0)continue;//skip the first argument
+        if (i == 1) {//consider second argument for the compiler command
+            if(strcmp(argv[i],"gcc")==0 || strcmp(argv[i],"cl")==0){
+                string_append_str(command, argv[i]);//append gcc command for the first argument
+            }
+            else{
+                printf("unsupported compiler '%s'\n",argv[i]);
+                free_string(&command);
+                exit(EXIT_FAILURE);
+            }
             continue;
         }
         size_t len_of_arg = strlen(argv[i]); // get len of the argv[i]
@@ -18,11 +27,21 @@ int main(int argc, char **argv) {
                 char* file_content=file_read_text(argv[i]);
                 if(file_content==NULL){ // if the file content is NULL exit the program
                     printf("unable to read file '%s'\n",argv[i]);
+                    free_string(&command);
                     exit(EXIT_FAILURE);
                 }
+                string* file_string=create_string(); //create file_string
+                if(string_append_str(file_string, file_content)){//append file_content to file_string, if unable to append exit the program
+                    printf("unable to create temporary file string\n");
+                    free_string(&command);
+                    free_string(&file_string);
+                    exit(EXIT_FAILURE);
+                }
+                free_string(&file_string);
             }
             else{
                 printf("file '%s' does not exist\n",argv[i]);
+                free_string(&command);
                 exit(EXIT_FAILURE);// if file does not exist exit the program
             }
         }
@@ -31,6 +50,6 @@ int main(int argc, char **argv) {
             string_append_str(command,argv[i]);//append the argv[i]
         }
     }
-    printf("final gcc command : '%s'\n",string_cstr(command));
+    printf("final compiler command : '%s'\n",string_cstr(command));
     free_string(&command);
 }
