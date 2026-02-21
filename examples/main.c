cd5193952ac33f1bc6eeaaeea2417c0fc1ab9483
bhanu <lorenbhanu@gmail.com>
2026-02-21 12:27:25 +0530

    added file downloading logic and replacing old include_url directive with actual downloaded file

--- examples/main.c
+++ examples/main.c
@@ -1,5 +1,5 @@
 #include <stdio.h>
-#include_url "https://github.com/l0r3nzoo/lorlib/blob/d716184f3de366af2e789786ac7279400915c4c1/file.c"
+#include_url "https://github.com/l0r3nzoo/lorlib/blob/d716184f3de366af2e789786ac7279400915c4c1/file.h"
 
 int main(){
     printf("Hello, World\n");
--- main.c
+++ main.c
@@ -6,7 +6,8 @@
 #include "string.h"
 #include "vector.h"
 
-#define TEMPFOLDER "gitcc/"
+#define TEMPDIR ".gitcc/"
+#define OFFSET 19
 
 int main(int argc, char **argv) {
     string *command = create_string();
@@ -36,17 +37,20 @@ int main(int argc, char **argv) {
                 size_t len_of_content=strlen(file_content);
                 if(len_of_content==0){// if the len of the file content is 0 skip to next iteration
                     printf("empty file passed skipping\n");
+                    free_string(&command);
                     free(file_content);
                     continue;
                 }
                 string* file_string=create_string();
                 if(file_string==NULL){//unable to create string, exit the program
+                    free_string(&command);
                     free(file_content);
                     exit(EXIT_FAILURE);
                 }
                 string_append_str(file_string,file_content);
                 vector* file_string_vector=string_split_char(file_string, '\n');
                 if(file_string_vector==NULL){//unable to create string vector, exit the program
+                    free_string(&command);
                     free_string(&file_string);
                     free(file_content);
                     exit(EXIT_FAILURE);
@@ -67,7 +71,78 @@ int main(int argc, char **argv) {
                         printf("captured url : %s\n",url_buffer);
 
                         //create directory path by removing domain from url_buffer
+                        char* directory_buffer=malloc(strlen(url_buffer)+1);
+                        char* destination_buffer=malloc(strlen(url_buffer)+1);
+                        memcpy(directory_buffer, url_buffer, strlen(url_buffer)+1);
+                        memcpy(destination_buffer, url_buffer, strlen(url_buffer)+1);
+                        memmove(directory_buffer,directory_buffer+OFFSET,strlen(directory_buffer) + 1 - OFFSET);
+                        memmove(destination_buffer,destination_buffer+OFFSET,strlen(destination_buffer) + 1 - OFFSET);
+                        string* directory_string = create_string();
+                        string_append_str(directory_string,directory_buffer);
+                        string* destination_string=create_string();
+                        string_append_str(destination_string, destination_buffer);
+                        string_prepend_str(destination_string, TEMPDIR);
+                        string_prepend_str(directory_string,TEMPDIR);
+                        char* temp_directory_buffer=(char*)string_cstr(directory_string);
+                        size_t lastindexofforwardslash=string_lastindexof_char(directory_string, '/');
+                        temp_directory_buffer[lastindexofforwardslash+1]='\0';
+
+
+                        dir_create_recursive((char*)string_cstr(directory_string));
+                        char* command_buffer=malloc(2048);
+                        sprintf(command_buffer, "powershell -Command \"Invoke-WebRequest -Uri '%s' -OutFile '%s'\"",url_buffer,string_cstr(destination_string));
+                        if(!file_exists(string_cstr(destination_string))){
+                            system(command_buffer);
+                        }
+                        if(file_exists(string_cstr(destination_string))){
+                            char* include_buffer=malloc(2048);
+                            sprintf(include_buffer, "#include \"%s\"", (char*)string_cstr(destination_string));
+                            if(string_replace_str(file_string, string_cstr(line), include_buffer)!=0){
+                                printf("unable to replace the directive\n");
+                                free(include_buffer);
+                                free_string(&command);
+                                free_string(&file_string);
+                                free(file_content);
+                                free_string(&directory_string);
+                                free_string(&destination_string);
+                                free(directory_buffer);
+                                free(destination_buffer);
+                                free(url_buffer);
+                                free(command_buffer);
+                                exit(EXIT_FAILURE);
+                            }
+                            string* file_path_string=create_string();
+                            string_append_str(file_path_string, argv[i]);
+                            size_t lastindexofforwardslash=string_lastindexof_char(file_path_string, '\\');
+                            string_remove_range(file_path_string, 0, lastindexofforwardslash+1);
+                            string_prepend_str(file_path_string, TEMPDIR);
+                            file_create(string_cstr(file_path_string));
+                            file_append_text(string_cstr(file_path_string), string_cstr(file_string));
+                            string_append_str(command," ");//add space
+                            string_append_str(command, string_cstr(file_path_string));//add the input file
+                            free_string(&file_path_string);
+                            free(include_buffer);
+                        }
+                        else{
+                            printf("unable to download the file : '%s'\n",url_buffer);
+                            free_string(&command);
+                            free_string(&file_string);
+                            free(file_content);
+                            free_string(&directory_string);
+                            free_string(&destination_string);
+                            free(directory_buffer);
+                            free(destination_buffer);
+                            free(url_buffer);
+                            free(command_buffer);
+                            exit(EXIT_FAILURE);
+                        }
+
+                        free_string(&directory_string);
+                        free_string(&destination_string);
+                        free(directory_buffer);
+                        free(destination_buffer);
                         free(url_buffer);
+                        free(command_buffer);
                     }
                 }
 
@@ -87,5 +162,6 @@ int main(int argc, char **argv) {
         }
     }
     printf("final compiler command : '%s'\n",string_cstr(command));
+    system(string_cstr(command));
     free_string(&command);
 }
